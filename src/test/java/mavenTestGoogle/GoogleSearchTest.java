c739be2b9e4a6ad7c014d3ad6c494932236a2a19
vivekunni007 <vivekunnikrishnan007@gmail.com>
2020-11-20 17:16:59 +0530

    First commit

---
+++ src/test/java/mavenTestGoogle/GoogleSearchTest.java
@@ -0,0 +1,20 @@
+package mavenTestGoogle;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+	@Test
+	public void GSearch(){
+	String exePath1 = "E:\\Softwares\\Selenium\\geckodriver\\geckodriver.exe";
+	 System.setProperty("webdriver.gecko.driver", exePath1);
+		WebDriver driver  = new FirefoxDriver();
+		driver.navigate().to("http://google.com");
+		String appTitle = driver.getTitle();
+		System.out.println("Application title is :: "+appTitle);
+		driver.quit();
+	}
+}
+
+
