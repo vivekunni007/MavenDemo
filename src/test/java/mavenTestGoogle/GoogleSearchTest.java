d80811cfba6ad588a019b57f6bec0f89f9c49d20
vivekunni007 <vivekunnikrishnan007@gmail.com>
2020-11-21 12:23:35 +0530

    Driver Updated

--- src/test/java/mavenTestGoogle/GoogleSearchTest.java
+++ src/test/java/mavenTestGoogle/GoogleSearchTest.java
@@ -1,15 +1,16 @@
 package mavenTestGoogle;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class GoogleSearchTest {
+	
 	@Test
 	public void GSearch(){
-	String exePath1 = "E:\\Softwares\\Selenium\\geckodriver\\geckodriver.exe";
-	 System.setProperty("webdriver.gecko.driver", exePath1);
-		WebDriver driver  = new FirefoxDriver();
+	String exePath1 = "E:\\Softwares\\Selenium\\chromedriver\\chromedriver.exe";
+	 System.setProperty("webdriver.chrome.driver", exePath1);
+		WebDriver driver  = new ChromeDriver();
 		driver.navigate().to("http://google.com");
 		String appTitle = driver.getTitle();
 		System.out.println("Application title is :: "+appTitle);
